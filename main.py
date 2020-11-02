b2e6537fbac9d881dc558657e7b3030b198c414c
Thiago Nascimento <nascimenthiago@gmail.com>
2020-11-02 13:26:53 -0300

    reading excel with pandas

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import pandas
+
+
+def read_excel_qualis():
+    excel_qualis = pandas.read_excel('data/qualis.xlsx', sheet_name='Qualis2019')
+    for row in excel_qualis.index:
+        print(excel_qualis['SIGLA'][row])
 
 
 if __name__ == '__main__':
-    print('Hello, world!')
+    read_excel_qualis()
